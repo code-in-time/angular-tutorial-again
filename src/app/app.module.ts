01d354fe8991a854b49f40eb3a387d3d10d6ace8
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-11-09 15:01:58 +0000

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppServerComponent } from './app-server/app-server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppServerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
